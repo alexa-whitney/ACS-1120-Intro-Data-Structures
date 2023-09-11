be935f50411870c045c541e770bae9e1588437a3
alexa-whitney <alexawhitney@gmail.com>
2023-09-10 20:07:47 -0600

    Added random and weighted to histogram module

--- Code/histogram.py
+++ Code/histogram.py
@@ -1,11 +1,17 @@
+import random
+import string
+
+
 def histogram(source_text):
     """Generate a histogram from a source text file."""
     with open(source_text, 'r', encoding='utf-8') as f:
         text = f.read().lower()
+        # Remove punctuation from text
+        translator = str.maketrans('', '', string.punctuation + '“”‘’')
+        text = text.translate(translator)
         words = text.split()
 
     hist = {}
-
 
     for word in words:
         if word in hist:
@@ -26,10 +32,41 @@ def frequency(word, hist):
     return hist.get(word, 0)
 
 
+def random_word(hist):
+    """Return a random word from the histogram, not based on frequency."""
+    return random.choice(list(hist.keys()))
+
+
+def weighted_random_word(hist):
+    """Return a random word from the histogram based on frequency."""
+    weighted_words = []
+    for word in hist:
+        for i in range(hist[word]):
+            weighted_words.append(word)
+    return random.choice(weighted_words)
+
+
 if __name__ == '__main__':
     source_path = "/Users/alexawhitney/Desktop/Dominican ACS Courses/Fall 2023/andersen_fairy_tales.txt"
     hist = histogram(source_path)
 
-    print("Total unique words:", unique_words(hist))
-    word_to_check = "prince"  # Change this to whatever word you want to check
-    print(f"Frequency of '{word_to_check}':", frequency(word_to_check, hist))
+    # Testing pure randomness:
+    print(random_word(hist))
+
+    # Testing weighted randomness:
+    test_runs = 10000
+    test_results = {}
+    for _ in range(test_runs):
+        word = weighted_random_word(hist)
+        test_results[word] = test_results.get(word, 0) + 1
+
+    # Display 20 random results:
+    sample_20_words = random.sample(list(test_results.keys()), 20)
+    for word in sample_20_words:
+        print(f"{word} => {test_results[word]}")
+
+    # Display the top 20 results:
+    top_20_words = sorted(
+        test_results, key=test_results.get, reverse=True)[:20]
+    for word in top_20_words:
+        print(f"{word} => {test_results[word]}")
