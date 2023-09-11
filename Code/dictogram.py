31763aefc936b5e9a4b0b6b92875b702d9ebda0f
alexa-whitney <alexawhitney@gmail.com>
2023-09-11 16:47:56 -0600

    Added dictogram and tests are passing

--- Code/dictogram.py
+++ Code/dictogram.py
@@ -20,16 +20,28 @@ class Dictogram(dict):
 
     def add_count(self, word, count=1):
         """Increase frequency count of given word by given count amount."""
-        # TODO: Increase word frequency by count
+        if word in self:
+            self[word] += count
+        else:
+            self[word] = count
+            self.types += 1
+        self.tokens += count
 
     def frequency(self, word):
         """Return frequency count of given word, or 0 if word is not found."""
-        # TODO: Retrieve word frequency count
+        if word in self:
+            return self[word]
+        else:
+            return 0
 
     def sample(self):
         """Return a word from this histogram, randomly sampled by weighting
         each word's probability of being chosen by its observed frequency."""
-        # TODO: Randomly choose a word based on its frequency in this histogram
+        # Randomly choose a word based on its frequency in this histogram
+        weighted_words_list = []
+        for word, frequency in self.items():
+            weighted_words_list.extend([word] * frequency)
+        return random.choice(weighted_words_list)
 
 
 def print_histogram(word_list):
